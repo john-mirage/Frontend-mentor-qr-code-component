7c258a5360bcf46a5d2b00a64fd6a0b1e5aae9c6
John Mirage <chaosdynamix@posteo.net>
2022-10-10 23:52:16 +0200

    Initial commit

---
+++ src/components/App/component.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from "react";
+
+const App: FunctionComponent = () => {
+  return (
+    <h1 className="font-bold mx-0 flex text-3xl underline">Hello world!</h1>
+  );
+};
+
+export default App;
