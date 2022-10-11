e9eb4fcb3176eb068fa053bb789418d65faed6e1
john-mirage <chaosdynamix@posteo.net>
2022-10-11 03:09:02 +0200

    fix: fix main landmark region

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,10 +3,10 @@ import { FunctionComponent } from "react";
 
 const App: FunctionComponent = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center px-24 py-48">
+    <main className="flex min-h-screen items-center justify-center px-24 py-48">
       <h1 className="sr-only">Qr code component</h1>
       <Card />
-    </div>
+    </main>
   );
 };
 
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,7 +3,7 @@ import qrCodeImage from "@images/qr-code.png";
 
 const Card: FunctionComponent = () => {
   return (
-    <main className="shadow-2xl w-full max-w-320 overflow-hidden rounded-20 bg-white px-16 pt-16 pb-40 text-center shadow-dark-blue/20">
+    <div className="shadow-2xl w-full max-w-320 overflow-hidden rounded-20 bg-white px-16 pt-16 pb-40 text-center shadow-dark-blue/20">
       <a
         className="aspect-w-1 aspect-h-1 mb-24 block overflow-hidden rounded-10 bg-blue shadow-card transition-shadow duration-100 hover-device:hover:shadow-card-hover"
         href="https://www.frontendmentor.io"
@@ -23,7 +23,7 @@ const Card: FunctionComponent = () => {
         Scan the QR code to visit Frontend Mentor and take your coding skills to
         the next level
       </p>
-    </main>
+    </div>
   );
 };
 
